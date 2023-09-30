2286f57d5c32ccdab39af62235d237d7ef892b7b
Darshan K.C <dkc1549@gmail.com>
2023-09-30 09:56:37 +0545

    aboutus

---
+++ aboutus.php
@@ -0,0 +1,11 @@
+<?php 
+require ('process/connect.php');
+include_once('include/header.php');
+include_once('include/navbar.php');
+?>
+<main>
+
+</main>
+<?php 
+include_once('include/footer.php');
+?>
